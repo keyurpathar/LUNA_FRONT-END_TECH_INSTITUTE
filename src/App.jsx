d0fc82bc5383f592fc15025290909417a8ca1ce5
Keyur Pathar <keyurpathar005@gmail.com>
2026-04-21 11:48:40 +0530

    feat : header and footer are only shown in userside not in admin side

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,25 @@ import Contact from './pages/Contact'
 import Login from './pages/Login'
 import SignUp from './pages/signUp'
 import { ReactLenis } from 'lenis/react'
+// import Dashboard from './pages/admin/Dashboard'
+import { useLocation } from "react-router-dom";
+// import CourseAdmin from './pages/admin/CourseAdmin'
 
 const App = () => {
+
+
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith("/admin");
+
   return (
     <ReactLenis root options={{ lerp: 0.05, wheelMultiplier: 1, smoothTouch: true }}>
       <div className='px-3 py-2 md:px-8 font-lato'>
-        <Header />
+        {!isAdmin && <Header />}
 
         <ScrollToTop />
 
         <Routes>
-          
+
           <Route path='/' element={<Home />} />
           <Route path='/course' element={<Courses />} />
           <Route path='/course/:id' element={<CourseDetail />} />
@@ -29,9 +37,23 @@ const App = () => {
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
 
+
+
         </Routes>
 
-        <Footer />
+        {!isAdmin && <Footer />}
+
+        {/*        <Routes>
+
+          admin routes 
+
+          <Route path='/admin' element={<Dashboard />} />
+           <Route path='/admin/course' element={<CourseAdmin />} />
+
+        </Routes> */}
+
+
+
       </div>
     </ReactLenis>
   )
